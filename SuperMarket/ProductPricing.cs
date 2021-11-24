f18752d7b55b2b14134a32ab710eb37497d7df60
yassine ghazzai <yassine.ghazzai@esprit.tn>
2021-11-24 12:35:14 +0100

    Create product catalog in a dictionary

--- SuperMarket/ProductPricing.cs
+++ SuperMarket/ProductPricing.cs
@@ -1,24 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace SuperMarket
 {
-     public class ProductPricing
+    public class ProductPricing
     {
+        private Dictionary<string, decimal> catalog;
+
+        public ProductPricing()
+        {
+            catalog = new Dictionary<string, decimal>() {
+                { "Product1", 0.65M },
+                { "Product2", 1.99M },
+                { "Product3", 3 }
+            };
+        }
         public decimal Pricing(string product)
         {
+            decimal price = -1;
             if (String.IsNullOrEmpty(product))
             {
                 return 0;
             }
-            return product switch
-            {
-                "Product1" => 0.65M,
-                "Product2" => 1.99M,
-                "Product3" => 3,
-                _ => -1,
-            };
+            catalog.TryGetValue(product, out price);
+            return price;
         }
 
     }
