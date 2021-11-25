8619dfc60cf2b8f6af8eca72580b21708c89c72b
yassine ghazzai <yassine.ghazzai@esprit.tn>
2021-11-25 14:24:53 +0100

    add test case for multiple products with discount

--- SuperMarket/ProductPricing.cs
+++ SuperMarket/ProductPricing.cs
@@ -1,25 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace SuperMarket
 {
     public class ProductPricing
     {
         private Dictionary<string, decimal> catalog;
-
+        private Dictionary<string, decimal[]> discounts;
         public ProductPricing()
         {
             catalog = new Dictionary<string, decimal>() {
                 { "Product1", 0.65M },
                 { "Product2", 1.99M },
-                { "Product3", 3 },
-                { "Product4", 10 }
-
+                { "Product3", 3 }
+            };
+            /* Our offers 3 of product1 cost 1.5 instead of 1.95 = discount of 0.45 
+                  2 of product3 cost 5 instead of 6 = discount of 1*/
+            discounts = new Dictionary<string, decimal[]>() {
+                { "Product1", new decimal[] { 3, 0.45M } },
+                { "Product3", new decimal[] { 2, 1 } }
             };
         }
         public decimal Pricing(string product)
         {
             decimal total = 0;
+            decimal totalDiscount = 0;
             if (String.IsNullOrEmpty(product))
             {
                 return total;
@@ -27,12 +33,11 @@ namespace SuperMarket
             /*split the input products into array of string to calculate the price 
             expecting the string product input to be separate the multiple products with a ","
             */
-            var items = product.Split(",");
-            foreach ( string item in items)
-            {
-                total = total + PriceFor(item);
-            }
-            return total;
+            var products = product.Split(",");
+            //code optimisation through using Linq instead of a foreach
+            total = products.Sum(x => PriceFor(x));
+            totalDiscount = discounts.Sum(discount => CalculateDiscount(discount.Key, discount.Value, products));
+            return total- totalDiscount;
         }
         private decimal PriceFor(string item)
         {
@@ -40,6 +45,10 @@ namespace SuperMarket
             catalog.TryGetValue(item, out price);
             return price;
         }
-
+        private decimal CalculateDiscount(string product, decimal[] discount, string[] products)
+        {
+            int itemCount = products.Count(item => item == product);
+            return itemCount == discount[0] || itemCount % discount[0]== 0 ? (itemCount / discount[0]) * discount[1] : 0;        
+        }
     }
 }
--- SuperMarket/ProductPricingTest.cs
+++ SuperMarket/ProductPricingTest.cs
@@ -19,12 +19,24 @@ namespace SuperMarket
             Assert.Equal(0, productPricing.Pricing(""));
         }
         [Theory]
-        [InlineData("Product1,Product4,Product2", 12.64)]
+        [InlineData("Product1,Product3,Product2", 5.64)]
         [InlineData("Product2,Product1", 2.64)]
         [InlineData("Product3", 3)]
-        public void OneProduct_Pricing(string product, decimal expected)
+        [InlineData("Product2", 1.99)]
+        public void MultipleProduct_Pricing_NoDicount(string product, decimal expected)
         {
             Assert.Equal(expected, productPricing.Pricing(product));
+        }
+        [Theory]
+        [InlineData("Product1,Product1,Product1", 1.5)]
+        [InlineData("Product1,Product1,Product1,Product2", 3.49)]
+        [InlineData("Product1,Product1,Product1,Product2,Product2", 5.48)]
+        [InlineData("Product1,Product1,Product1,Product1,Product1,Product1", 3)]
+        [InlineData("Product1,Product1,Product1,Product3,Product3", 6.5)]
+        [InlineData("Product3,Product3", 5)]
+        public void Pricing_discounted_combinations_Products(string scan, decimal expected)
+        {
+            Assert.Equal(expected, productPricing.Pricing(scan));
         }
     }
 }
